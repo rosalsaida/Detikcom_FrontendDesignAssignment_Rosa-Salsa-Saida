267a0ddaf2fe7da3ed26bead0d56b75af8403b1d
rosalsaida <rosalsaida82@gmail.com>
2023-07-22 12:01:10 +0700

    update responsive & javascript

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function register_competition() {
+    var nama_perusahaan = document.getElementById('nama-perusahaan').value;
+    var email = document.getElementById('email').value;
+    var no_tlp = document.getElementById('noTlp').value;
+    var tentang_perusahaan = document.getElementById('tentangPerusahaan').value;
+    var logo_perusahaan = document.getElementById('user-upload').value;
+    var profil_perusahaan = document.getElementById('user-upload1').value;
+    var link_perusahaan = document.getElementById('linkPerusahaan').value;
+}
